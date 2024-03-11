4dbe25a27191ae0143cdb93517dcebdc56d74149
camisanabriam <acsanabriam@unal.edu.co>
2024-03-11 10:37:55 -0500

    updated

--- R/Datavailability.R
+++ R/Datavailability.R
@@ -2,54 +2,73 @@
 #'
 #' @param time_series_df 
 #' @param station_info_df 
-#' @param output_dir
+#' @param output_dir 
+#' @param plot 
+#' @param resol 
+#' @param var.name 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-station_data_av<- function(time_series_df, station_info_df, output_dir = "./") {
+#' 
+station_data_av<- function(time_series_df, station_info_df, output_dir = "./",plot = TRUE, resol = 400,
+                           var.name = "Prec (mm)", xcluster = TRUE) {
+  
   # Ensure the 'date' column is in Date format
-  time_series_df$date <- as.Date(time_series_df$date)
+  time_series_df[[1]] <- as.Date(time_series_df[[1]])
   
   # Transpose the time_series_df to have station codes as rows
   transposed_df <- time_series_df %>%
-    gather(key = "station_code", value = "value", -date)
+    gather(key = "station_code", value = "value", -dates)
   
   # Merge the transposed dataframe with station_info_df on station code
-  merged_df <- merge(transposed_df, station_info_df, by.x = "station_code", by.y = "code")
+  merged_df <- merge(transposed_df, station_info_df, by.x = "station_code", by.y = "code", all.x =TRUE)
   
-  # Order stations by latitude
-  station_order <- merged_df %>%
-    select(station_code, latitude, cluster) %>%
-    distinct() %>%
-    arrange(cluster, latitude) %>%
-    pull(station_code)
+  if(xcluster){
+    
+    # Order stations by latitude
+    station_order <- merged_df %>%
+      select(station_code, latitude, cluster) %>%
+      distinct() %>%
+      arrange(cluster, latitude) %>%
+      pull(station_code)
+    
+    # Step: Calculate the number of available data points each day in all stations
+    availability_data <- merged_df %>%
+      group_by(dates, cluster) %>%
+      summarise(available_count = sum(!is.na(value)))
+    
+  }else{
+    
+    availability_data <- merged_df %>%
+      group_by(dates) %>%
+      summarise(available_count = sum(!is.na(value)))
+  }
   
-  # Step: Calculate the number of available data points each day in all stations
-  availability_data <- merged_df %>%
-    group_by(date, cluster) %>%
-    summarise(available_count = sum(!is.na(value)))
   
-  # Create a line plot for data availability with subplots for each cluster
-  availability_plot <- ggplot(availability_data, aes(x = date, y = available_count)) +
-    geom_line(color = "blue", linewidth = 0.3) +
-    labs(title = "Data Availability Over Time",
-         x = "Date",
-         y = "Number of Available Data Stations") +
-    theme_bw()+
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.background = element_blank()) +
-    #facet_wrap(~cluster, scales = "free_y", ncol = 2) +  # Subplots for each cluster
-    theme(panel.border = element_rect(colour = "black", fill=NA, size=1)) +
-    theme(axis.text.x = element_text(angle = 0, hjust = 1))+
-    theme(panel.spacing = unit(0.5, "cm"), axis.text.y = element_text(size = 8))+
-    scale_x_date(breaks = seq(min(availability_data$date), max(availability_data$date), by = "3 month"), date_labels = "%Y")  # Adjust the breaks for x-axis ticks
-
+  if(!plot){
+  }else{
+    
+    # Create a line plot for data availability with subplots for each cluster
+    availability_plot <- ggplot(availability_data, aes(x = dates, y = available_count)) +
+      geom_line(color = "blue", linewidth = 0.3) +
+      labs(title = "Data Availability Over Time",
+           x = "Date",
+           y = "Number of Available Data Stations") +
+      theme_bw()+
+      theme(axis.line = element_line(colour = "black"),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank()) +
+      {if(xcluster)facet_wrap(~cluster, scales = "free_y", ncol = 2)}+# Facet by cluster, allowing y-axis scales to vary
+      theme(panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 0.5))+
+      theme(panel.spacing = unit(0.5, "cm"), axis.text.y = element_text(size = 8))+
+      scale_x_date(breaks = seq(min(availability_data$dates), max(availability_data$dates), by = "60 month"), date_labels = "%Y")  # Adjust the breaks for x-axis ticks
   
   # Save the combined plot as a JPEG file
-  ggsave(file.path(output_dir, "stations_Availability.jpeg"),availability_plot , width = 22, height = 15, units = "cm", dpi = 400)
+  ggsave(file.path(paste0(output_dir,var.name,"_sta_av.jpeg")),availability_plot , width = 22, height = 12, units = "cm", dpi = resol)
+  }
   
 }
--- R/heatmap_per_cluster.R
+++ R/heatmap_per_cluster.R
@@ -2,56 +2,82 @@
 #'
 #' @param time_series_df 
 #' @param station_info_df 
-#' @param output_dir
+#' @param output_dir 
+#' @param cluster 
+#' @param resol 
+#' @param var.name 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-missing_data_heatmap <- function(time_series_df, station_info_df, output_dir = "./") {
+#' 
+missing_data_heatmap <- function(time_series_df, station_info_df, output_dir = "./",xcluster = TRUE, resol = 400, var.name = "Precipitation (mm)") {
+  
   # Ensure the 'date' column is in Date format
-  time_series_df$date <- as.Date(time_series_df$date)
+  time_series_df[[1]] <- as.Date(time_series_df[[1]])
+  
+  
   
   # Transpose the time_series_df to have station codes as rows
   transposed_df <- time_series_df %>%
-    gather(key = "station_code", value = "value", -date)
+    gather(key = "station_code", value = "value", -dates)
   
   # Merge the transposed dataframe with station_info_df on station code
-  merged_df <- merge(transposed_df, station_info_df, by.x = "station_code", by.y = "code")
+  merged_df <- merge(transposed_df, station_info_df, by.x = "station_code", by.y = "code",all.x=TRUE)
   
-  # Order stations by latitude
-  station_order <- merged_df %>%
-    select(station_code, latitude, cluster) %>%
-    distinct() %>%
-    arrange(cluster, latitude) %>%
-    pull(station_code)
+  if(xcluster){
+    # Order stations by latitude
+    station_order <- merged_df %>%
+      select(station_code, latitude, cluster) %>%
+      distinct() %>%
+      arrange(cluster, latitude) %>%
+      pull(station_code)
+    
+    # Estimate missing data for each month on each station
+    missing_data <- merged_df %>%
+      group_by(station_code, month = format(dates, "%Y-%m"), cluster) %>%
+      summarise(missing_count = sum(is.na(value)))
+    
+  }else{
+
+    # Order stations by latitude
+    station_order <- merged_df %>%
+      select(station_code, latitude) %>%
+      distinct() %>%
+      arrange(latitude) %>%
+      pull(station_code)
+    
+    missing_data <- transposed_df %>%
+      group_by(station_code, month = format(dates, "%Y-%m")) %>%
+      summarise(missing_count = sum(is.na(value)))
+
+  }
   
-  # Estimate missing data for each month on each station
-  missing_data <- merged_df %>%
-    group_by(station_code, month = format(date, "%Y-%m"), cluster) %>%
-    summarise(missing_count = sum(is.na(value)))
+  max_value <- max(missing_data$missing_count, na.rm = TRUE)
+  missing_data$group <- cut(missing_data$missing_count, breaks = seq(0, 30 + 5, by = 5), include.lowest = T, labels = c("0", "1 - 5", "6 - 10", "11 - 15","16 - 20","21 - 25",">26"))
   
   # Create a single plot with subplots for each cluster (missing data)
-  heatmap_plot <- ggplot(missing_data, aes(x = month, y = factor(station_code, levels = station_order), fill = factor(missing_count))) +
+  heatmap_plot <- ggplot(missing_data, aes(x = month, y = factor(station_code, levels = station_order), fill = factor(group))) +
     geom_tile() +
-    scale_fill_brewer(palette = "YlGnBu",)+
+    scale_fill_brewer(palette = "YlGnBu") +
     theme_bw()+
     theme(axis.line = element_line(colour = "black"),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           panel.background = element_blank()) +
     theme(legend.text = element_text(size = 5), legend.title = element_text(size = 5)) +
-    labs(title = "Missing Data Heatmap",
+    labs(title = paste0("Missing Data - ",var.name),
          x = "Month",
          y = "Station Code",
          fill = "Missing Count") +
-    theme(panel.border = element_rect(colour = "black", fill=NA, size=1)) +
-    theme(panel.spacing = unit(0.5, "cm"), axis.text.y = element_text(size = 8))+
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-    facet_wrap(~cluster, scales = "free_y", ncol = 2)+# Facet by cluster, allowing y-axis scales to vary
-    scale_x_discrete(breaks = unique(missing_data$month)[seq(1, length(unique(missing_data$month)), by = 12)]) 
-  
+    theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = .6)) +
+    theme(panel.spacing = unit(0.5, "cm"), axis.text.y = element_text(size = 5))+
+    theme(axis.text.x = element_text(angle = 90, size = 5)) +
+    {if(xcluster)facet_wrap(~cluster, scales = "free_y", ncol = 2)}+# Facet by cluster, allowing y-axis scales to vary
+    scale_x_discrete(breaks = unique(missing_data$month)[seq(1, length(unique(missing_data$month)), by = 48)])+
+    scale_y_discrete(breaks = station_order[seq(1, length(station_order), by = 2)]) 
   # Save the combined plot as a JPEG file
-  ggsave(file.path(output_dir, "combined_heatmap.jpeg"), heatmap_plot, width = 22, height = 15, units = "cm", dpi = 400)
-
+  ggsave(file.path(paste0(output_dir,var.name, "_avheatmap.jpeg")), heatmap_plot, width = 22, height = 15, units = "cm", dpi = resol)
+  
 }
