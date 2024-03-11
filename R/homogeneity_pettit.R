4762cecfe42ffbc0d899504242803beb64b41fbe
camisanabriam <acsanabriam@unal.edu.co>
2024-03-11 10:28:08 -0500

    updated

--- R/homogeneity_pettit.R
+++ R/homogeneity_pettit.R
@@ -1,21 +1,25 @@
 
 #' Title
 #'
-#' @param df 
+#' @param time_series_df 
+#' @param station_info_df 
+#' @param resol 
+#' @param var.name 
+#' @param output_dir 
+#' @param plot 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' 
-gra_HOMO_Pettitt_multi_columns <- function(df) {
+gra_Pettitt <- function(time_series_df,station_info_df, resol = 400, var.name = "Precipitation (mm)",output_dir = "./",
+                        xcluster=TRUE, plot=FALSE) {
   results_list <- list()  # List to store results for each column
   
-  for (col in names(df)[-1]) {  # Exclude the Date column from processing
+  for (col in names(time_series_df)[-1]) {  # Exclude the Date column from processing
     est <- col
     
-    
-    filtered <- na.omit(df[,c("date",col)])  # Filter out rows with missing values
+    filtered <- na.omit(time_series_df[,c("date",col)])  # Filter out rows with missing values
     
     pettitt_res <- pettitt.test(filtered[[col]]) # Perform Pettitt test
     
@@ -38,41 +42,83 @@ gra_HOMO_Pettitt_multi_columns <- function(df) {
       'significant' = significant
     )
     
-    maver <- ma(df[[col]],win.len = 12, FUN = mean, na.rm = F)# Calculate moving average
+    maver <- ma(time_series_df[[col]],win.len = 12, FUN = mean, na.rm = F)# Calculate moving average
     
-    # Set up PNG file for each column plot
-    png(sprintf("11plot_%s.png", est),width = 20, height = 10, units = 'cm', res = 400)  # Change 'pdf' to 'png' if you prefer a different format
-    
-    # Visualization (you can customize this part as needed)
-    title <- sprintf("Pettitt test - Station: %s", est)
-    label_x <- sprintf('Date range: %s to %s', format(min(filtered$date), "%Y-%m-%d"), format(max(filtered$date), "%Y-%m-%d"))
-    label_y <- "Precipitation [mm]"
-    
-    par(mar = c(2, 2, 1, 1))  # Adjust margins (bottom, left, top, right)
-    par(mgp = c(1, 0.5, 0))  # Adjust the distance between the axis label and the axis 
-    
-    plot(df$date, df[[col]], type = 'l', col = '#f0eded', xlab = label_x, ylab = label_y, main = title,
-         cex.main = 0.5, cex.lab = 0.5, cex.axis = 0.5 )
-    
-    # Add lines using segments to control xlim
-    segments(min(filtered$date), mu1, loc, mu1, col = 'orange', lty = 2, lw = 1)
-    segments(loc, mu2, max(filtered$date), mu2, col = '#336334', lty = 2, lw = 1)
-    abline(v = as.numeric(loc), col = 'red', lty = 3, lw = 2)
-    
-    lines(df$date, maver, col = 'blue', lty = 1) # Plotting moving average
-
-    legend("topright", legend = c('Observed',  'm. Average',sprintf('ave 1: %.2fmm', mu1), sprintf('ave 2: %.2fmm', mu2), 
-                                  sprintf('Change point: %s', format(loc, "%Y-%m-%d"))),
-           col = c('#f0eded','blue', 'orange', '#336334', 'red'),
-           lty = c(1,1, 2, 2, 3), cex = 0.5) # Add legend
-    
-    dev.off()# Close the PNG file
+    if(!plot){
+    }else{
+      
+      # Set up PNG file for each column plot
+      png(paste0(output_dir,sprintf("homopet_%s.png", est)),width = 20, height = 10, units = 'cm', res = resol)  # Change 'pdf' to 'png' if you prefer a different format
+      
+      # Visualization (you can customize this part as needed)
+      title <- sprintf("Pettitt test - Station: %s", est)
+      label_x <- sprintf('Date range: %s to %s', format(min(filtered$date), "%Y-%m-%d"), format(max(filtered$date), "%Y-%m-%d"))
+      label_y <- var.name
+      
+      par(mar = c(2, 2, 1, 1))  # Adjust margins (bottom, left, top, right)
+      par(mgp = c(1, 0.5, 0))  # Adjust the distance between the axis label and the axis 
+      
+      plot(time_series_df$date, time_series_df[[col]], type = 'l', col = '#f0eded', xlab = label_x, ylab = label_y, main = title,
+           cex.main = 0.5, cex.lab = 0.5, cex.axis = 0.5 )
+      
+      lines(time_series_df$date, maver, col = 'blue', lty = 1) # Plotting moving average
+      
+      # Add lines using segments to control xlim
+      segments(min(filtered$date), mu1, loc, mu1, col = 'orange', lty = 2, lw = 1)
+      segments(loc, mu2, max(filtered$date), mu2, col = '#336334', lty = 2, lw = 1)
+      abline(v = as.numeric(loc), col = 'red', lty = 3, lw = 1.5)
+      
+      legend("topright", legend = c('Observed',  'm. Average',sprintf('ave 1: %.2f', mu1), sprintf('ave 2: %.2f', mu2), 
+                                    sprintf('Change point: %s', format(loc, "%Y-%m-%d"))),
+             col = c('#f0eded','blue', 'orange', '#336334', 'red'),
+             lty = c(1,1, 2, 2, 3), cex = 0.5) # Add legend
+      
+      dev.off()# Close the PNG file
+    }
     
   }
   
   # Convert the results list to a data frame
   results_df <- as.data.frame(do.call(rbind, results_list), stringsAsFactors = FALSE)
   
+  if(!xcluster){
+  }else{
+  
+    results_df$code<-row.names(results_df)
+    time_ser_df <-as.data.frame(merge(results_df, station_info_df, by= "code",all.x=TRUE)) %>%
+      select(code,significant,cluster)
+    
+    # Convert each element in the "significant" list to a factor
+    time_ser_df$significant <- sapply(time_ser_df$significant, function(x) as.factor(unlist(x)))
+    
+    # Create a data frame for ggplot
+    ggplot_data <- data.frame(cluster = rep(time_ser_df$cluster, sapply(time_ser_df$significant, length)),
+                              significant = unlist(time_ser_df$significant))
+    
+    # Create the pie chart with labels
+    pie_chart <- ggplot(ggplot_data, aes(x = "", fill = significant)) +
+      geom_bar(stat = "count", position = "fill", width = 1) +
+      coord_polar(theta = "y") +
+      facet_wrap(~ cluster) +
+      theme_bw() +
+      theme(
+        axis.text = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        legend.position = "bottom"
+      ) +
+      scale_fill_manual(values = c("lightseagreen", "purple")) +
+      geom_text(stat = "count", aes(label = scales::percent(..count.. / sum(..count..))), position = position_fill(vjust = 0.5), show.legend = FALSE) +
+      labs(title = "Significance Pie Chart by Cluster")
+    
+    
+    plot(pie_chart)
+    ggsave(file.path(paste0(output_dir,var.name,"piechart.jpeg")),pie_chart, width = 22, height = 15, units = "cm", dpi = resol)
+    dev.off()
+  }
+  
   return(results_df)
+  
 }
 
